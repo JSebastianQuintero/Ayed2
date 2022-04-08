371390ce6570e10eb7d80ec6408c665251d910b1
Juan Quintero <quinterojuan0246@gmail.com>
2022-04-08 19:52:38 -0300

    ej 1 resuelto

--- lab01/ej1/main.c
+++ lab01/ej1/main.c
@@ -45,11 +45,24 @@ unsigned int array_from_file(int array[],
            unsigned int max_size,
            const char *filepath) {
     //your code here!!!
-
+    int tmp; 
+    FILE * file = fopen(filepath, "r");
+    if (file == NULL) return 0;         // check file
+    fscanf(file, "%i", &tmp);           // first number = array lenght
+    if (tmp>max_size) return 0;         // check array lenght < max_side
+    for(unsigned int i=0; i<tmp; i++){
+        fscanf(file, "%i", &array[i]);  // reading and saving the array values from file
+    }
 }
 
 void array_dump(int a[], unsigned int length) {
     //your code here!!!!!
+    printf("[");
+    for(unsigned int i=0; i<length; i++){
+        printf(" %i", a[i]);
+        if(i<length-1) printf(",");
+    }
+    printf("]\n");
 }
 
 
