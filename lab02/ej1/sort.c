4f4a74fac05ac8650823113e84dd5bf0b2a55bca
Juan Quintero <quinterojuan0246@gmail.com>
2022-04-26 14:15:38 -0300

    ej1

--- lab02/ej1/sort.c
+++ lab02/ej1/sort.c
@@ -9,11 +9,16 @@
 
 static void insert(int a[], unsigned int i) {
     /* needs implementation */
+    for (unsigned int j = i; j > 0; j--){
+        if (goes_before(a[j], a[j-1])) swap(a, j-1, j);
+    }
+    
 }
 
 void insertion_sort(int a[], unsigned int length) {
     for (unsigned int i = 1u; i < length; ++i) {
         /* needs implementation */
         insert(a, i);
+        assert(array_is_sorted(a, i));
     }
 }
