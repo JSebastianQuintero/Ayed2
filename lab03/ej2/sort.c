d1822203ca57d9230144281ad46a8526bf9a58ce
Juan Quintero <quinterojuan0246@gmail.com>
2022-04-27 15:06:10 -0300

    ej2

--- lab03/ej2/sort.c
+++ lab03/ej2/sort.c
@@ -11,10 +11,46 @@
 #include "sort.h"
 #include "player.h"
 
+static void swap(player_t a[], unsigned int i, unsigned int j){
+    player_t aux = a[i];
+    a[i] = a[j];
+    a[j] = aux;
+}
+
 bool goes_before(player_t x, player_t y){
     // completar aquí
-    return true;
+    return (x.rank<=y.rank);
 }
+
+static unsigned int partition(player_t a[], unsigned int izq, unsigned int der) {
+   unsigned int i, j, ppiv;
+   ppiv = izq;
+   i = izq + 1;
+   j = der;
+   while (i<=j){
+       if (goes_before(a[i], a[ppiv])){
+           i++;
+       }else if(goes_before(a[ppiv], a[j])){
+           j--;
+       }else if(goes_before(a[ppiv], a[i]) && goes_before(a[j], a[ppiv])) swap(a, i, j);
+   }
+   swap(a, ppiv, j);
+   ppiv = j;
+   return ppiv;
+}
+
+static void quick_sort_rec(player_t a[], unsigned int izq, unsigned int der) {
+    if (der > izq){
+        unsigned int ppiv = partition(a, izq, der);
+        quick_sort_rec(a, izq, (ppiv == 0u) ? 0u : ppiv - 1u);
+        quick_sort_rec(a, ppiv+1, der);
+    }
+}
+
+void quick_sort(player_t a[], unsigned int length) {
+    quick_sort_rec(a, 0, (length == 0) ? 0 : length - 1);
+}
+
 
 bool array_is_sorted(player_t atp[], unsigned int length) {
     unsigned int i = 1u;
@@ -26,5 +62,5 @@ bool array_is_sorted(player_t atp[], unsigned int length) {
 
 void sort(player_t a[], unsigned int length) {
     // completar aquí
+    quick_sort(a, length);
 }
-
