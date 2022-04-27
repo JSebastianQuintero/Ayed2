fa640d6a89231934b65e4f55daae5c0e08335a17
Juan Quintero <quinterojuan0246@gmail.com>
2022-04-27 13:21:39 -0300

    ej0

--- lab03/ej0/main.c
+++ lab03/ej0/main.c
@@ -3,6 +3,21 @@
 
 #define MAX_SIZE 1000
 
+static unsigned int data_from_file(const char *path,
+                            unsigned int indexes[],
+                            char letters[],
+                            unsigned int max_size){
+    FILE *file;
+    file = fopen(path, "r");
+    unsigned int length=0u;
+    while(!feof(file)){
+        fscanf(file, "%u '%c'\n", &indexes[length], &letters[length]);
+        length++;
+        if (length==max_size) break;
+    }
+    return length;
+}
+
 static void dump(char a[], unsigned int length) {
     printf("\"");
     for (unsigned int j=0u; j < length; j++) {
@@ -12,18 +27,26 @@ static void dump(char a[], unsigned int length) {
     printf("\n\n");
 }
 
-int main(/* -- completar-- */) {
-    FILE *file;
+int main(int argc, char *argv[]) {
+
     unsigned int indexes[MAX_SIZE];
     char letters[MAX_SIZE];
     char sorted[MAX_SIZE];
-    unsigned int length=0; 
+    
+    if (argc < 2) {
+        printf("Falta path\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    unsigned int length=data_from_file(argv[1], indexes, letters, MAX_SIZE); 
     //  .----------^
     //  :
     // Debe guardarse aqui la cantidad de elementos leidos del archivo
     
     /* -- completar -- */
-
+    for(unsigned int i=0u; i<length; i++){
+        sorted[indexes[i]]=letters[i];
+    }
     dump(sorted, length);
 
     return EXIT_SUCCESS;
