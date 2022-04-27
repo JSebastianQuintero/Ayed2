e770be41d056fc4b9826f0433eafe03539ee5e21
Juan Quintero <quinterojuan0246@gmail.com>
2022-04-27 15:50:04 -0300

    ej4

--- lab03/ej4/proc1.c
+++ lab03/ej4/proc1.c
@@ -3,10 +3,18 @@
 
 void absolute(int x, int y) {
     // Completar aqui
+    if(x >= 0){
+        y=x;
+    }else{
+        y=-x;
+    }
 }
 
 int main(void) {
     // Completar aqui
+    int x=-98, y=0;
+    absolute(x, y);
+    printf("%d", y);
     return EXIT_SUCCESS;
 }
 
